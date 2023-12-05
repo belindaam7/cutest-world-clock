d9b475e39f3fa35fda523f32c040382ccabeb60b
belindaam7 <bsamezquita@gmail.com>
2023-12-04 22:39:39 -0800

    Java dropdown selector
    Added multiple city options to replace inner HTML with current time for the selected time zone.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
 setInterval(function () {
-  let sanDiegoElement = document.querySelector("#san-diego");
-  let sanDiegoDateElement = sanDiegoElement.querySelector(".date");
-  let sanDiegoTimeElement = sanDiegoElement.querySelector(".time");
-  let sanDiegoTime = moment().tz("America/Tijuana");
+  let tijuanaElement = document.querySelector("#san-diego");
+  if (tijuanaElement) {
+    let tijuanaDateElement = tijuanaElement.querySelector(".date");
+    let tijuanaTimeElement = tijuanaElement.querySelector(".time");
+    let tijuanaTime = moment().tz("America/Tijuana");
 
-  sanDiegoDateElement.innerHTML = moment().format("MMMM Do, YYYY");
-  sanDiegoTimeElement.innerHTML = sanDiegoTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-
-  let timbuktuElement = document.querySelector("#timbuktu");
-  let timbuktuDateElement = timbuktuElement.querySelector(".date");
-  let timbuktuTimeElement = timbuktuElement.querySelector(".time");
-  let timbuktuTime = moment().tz("Africa/Timbuktu");
-
-  timbuktuDateElement.innerHTML = moment().format("MMMM Do, YYYY");
-  timbuktuTimeElement.innerHTML = timbuktuTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    tijuanaDateElement.innerHTML = moment()
+      .tz("America/Tijuana")
+      .format("MMMM Do, YYYY");
+    tijuanaTimeElement.innerHTML = tijuanaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 });
+
+setInterval(function () {
+  let timbuktuElement = document.querySelector("#timbuktu");
+  if (timbuktuElement) {
+    let timbuktuDateElement = timbuktuElement.querySelector(".date");
+    let timbuktuTimeElement = timbuktuElement.querySelector(".time");
+    let timbuktuTime = moment().tz("Africa/Timbuktu");
+
+    timbuktuDateElement.innerHTML = moment()
+      .tz("Africa/Timbuktu")
+      .format("MMMM Do, YYYY");
+    timbuktuTimeElement.innerHTML = timbuktuTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+});
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+
+        <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+      </div>
+      <div class="time">
+        ${cityTime.format("h:mm:ss")}
+        <small>${cityTime.format("A")}</small>
+      </div>
+    </div>`;
+}
+
+let citySelectElement = document.querySelector("#city-selector");
+citySelectElement.addEventListener("change", updateCity);
